78cad8d1658ebbb6c292a981c86afd10dc013efd
Haniyeh Rezaei <haniyehrezaei344@gmail.com>
2025-10-28 01:02:31 +0330

    SE_P_4:Adding a package to receive input from user

---
+++ models/UserRole.java
@@ -0,0 +1,14 @@
+package models;
+/*
+        * مدیر سیستم
+ */
+public class Admin extends User {
+    private String name;
+
+    public Admin(String username, String password, String name) {
+        super(username, password);
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+}
